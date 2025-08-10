acf0a49a86cda591212f2a759b222994dfb6cc00
Hizuro <hizuro@gmx.net>
2025-08-10 14:47:18 +0200

    Fix little typo

--- LFR_of_the_past.lua
+++ LFR_of_the_past.lua
@@ -659,7 +659,7 @@ local function addWaypointToOpt(opt,npc)
 	}
 	opt.args.MapPin = {
 		type = "execute", order = 4, width="half",
-		name = L["MapPin"],.--desc = L["MapPinDesc"],
+		name = L["MapPin"], --desc = L["MapPinDesc"],
 		func = function()
 			HST.AddWaypoint(npc[2],npc[3],npc[4],L["NPC"..npc[1]],addon,false,false)
 		end
